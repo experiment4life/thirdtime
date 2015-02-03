cd6678f69725dc7c7756f23e4c6a212e0063bcc9
experiment4life <mcakirby@yahoo.co.uk>
2015-02-03 13:18:03 -0600

    added pins via scaffolding and db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129204309) do
+ActiveRecord::Schema.define(version: 20150202183416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20150129204309) do
     t.integer "age",                null: false
     t.string  "address", limit: 50
     t.float   "salary"
+  end
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
